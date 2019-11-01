c8d00aad3fb0a1cd6df63ee2f23f775aadcb3c16
adampy <37088720+adampy@users.noreply.github.com>
2019-11-01 15:24:49 +0000

    Add files via upload

---
+++ Graph Algorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Math;
+
+namespace Graph_Algorithms
+{
+    class Vector
+    {
+        int i; //horizontal
+        int j; //vertical
+
+        public Vector(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+        }
+
+        public double Distance()
+        {
+            double x = Math.Pow(this.i, 2) + Math.Pow(this.j, 2);
+            double distance = Math.Sqrt(x);
+            return distance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vec1 = new Vector(2, 4);
+            Console.WriteLine(vec1.Distance());
+        }
+    }
+}
